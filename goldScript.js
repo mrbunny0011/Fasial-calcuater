955ee2e8bdf5a30f588e44a6079258abf1d4ecab
mrbunny0011 <basitbunny22@gmail.com>
2025-02-02 19:56:38 +0500

    Added new files and updated existing code

--- goldScript.js
+++ goldScript.js
@@ -30,29 +30,37 @@ btn.addEventListener("click", () => {
     // Logging results
     let ans_p =calcP(h, l, c);
     let ans_r1 =R1(h, l, c);
-    let ans_s1 =S1(h, l, c);
     let ans_r2 =R2(h, l, c);
+    let ans_r3 =R3(h, l, c);
+    let ans_s1 =S1(h, l, c);
     let ans_s2 =S2(h, l, c);
+    let ans_s3 =S3(h, l, c);
     //Center result
     let ans_C_of_P_1R=C_of_P_1R(ans_p,ans_r1);
-    let ans_C_of_P_1S=C_of_P_1S(ans_p,ans_s1);
     let ans_C_of_1R_2R=C_of_1R_2R(ans_r1,ans_r2);
+    let ans_C_of_2R_3R=C_of_2R_3R(ans_r2,ans_r3);
+    let ans_C_of_P_1S=C_of_P_1S(ans_p,ans_s1);
     let ans_C_of_1S_2S=C_of_1S_2S(ans_s1,ans_s2);
+    let ans_C_of_2S_3S=C_of_2S_3S(ans_s2,ans_s3);
     
 
     //print
     const peint_name = document.querySelector(".name").textContent=`Calculation of ${name}`; // Assuming this is for display
-    const peint_P = document.querySelector(".P").textContent=` ${ans_p}`;
     const peint_1R = document.querySelector(".R1").textContent=` ${ans_r1}`;
-    const peint_1S = document.querySelector(".S1").textContent=` ${ans_s1}`;
     const peint_2R = document.querySelector(".R2").textContent=` ${ans_r2}`;
+    const peint_3R = document.querySelector(".R3").textContent=` ${ans_r3}`;
+    const peint_P = document.querySelector(".P").textContent=` ${ans_p}`;
+    const peint_1S = document.querySelector(".S1").textContent=` ${ans_s1}`;
     const peint_2S = document.querySelector(".S2").textContent=` ${ans_s2}`;
-
+    const peint_3S = document.querySelector(".S3").textContent=` ${ans_s3}`;
+    //center
     const peint_ans_C_of_P_1R = document.querySelector(".c_of_p_r1").textContent=` ${ans_C_of_P_1R}`;
-    const peint_ans_C_of_P_1S = document.querySelector(".c_of_p_s1").textContent=` ${ans_C_of_P_1S}`;
-    const peint_P1 = document.querySelector(".P1").textContent=` ${ans_p}`;
     const peint_ans_C_of_1R_2R = document.querySelector(".c_of_r1_r2").textContent=` ${ans_C_of_1R_2R}`;
+    const peint_ans_C_of_2R_3R = document.querySelector(".c_of_r2_r3").textContent=` ${ans_C_of_2R_3R}`;
+    const peint_P1 = document.querySelector(".P1").textContent=` ${ans_p}`;
+    const peint_ans_C_of_P_1S = document.querySelector(".c_of_p_s1").textContent=` ${ans_C_of_P_1S}`;
     const peint_ans_C_of_1S_2S = document.querySelector(".c_of_s1_s2").textContent=` ${ans_C_of_1S_2S}`;
+    const peint_ans_C_of_2S_3S = document.querySelector(".c_of_s2_s3").textContent=` ${ans_C_of_2S_3S}`;
    
    
 });
@@ -76,28 +84,47 @@ function C_of_1R_2R(R1,R2){
     return ans;
 }
 
+function C_of_2R_3R(R2,R3){
+    
+    let ans= (R2+R3)/2;
+    return ans;
+}
+
 function C_of_1S_2S(S1,S2){
     let ans= (S1+S2)/2;
     return ans;
 }
 
+function C_of_2S_3S(S2,S3){
+    let ans= (S2+S3)/2;
+    return ans;
+}
+
 
 // Functions
+//R
 function R1(h, l, c) {
     const p = calcP(h, l, c);
     const ans = (2 * p) - l;
     return ans;
 }
 
-function S1(h, l, c) {
-    const p = calcP(h, l, c);
-    const ans = (2 * p) - h;
-    return ans;
-}
 
 function R2(h, l, c) {
     const p = calcP(h, l, c);
     const ans = (p + h) - l;
+    return ans;
+}
+
+function R3(h, l, c) {
+    const p = calcP(h, l, c);
+    const ans = 2*(p - l) + h;
+    return ans;
+}
+//S
+function S1(h, l, c) {
+    const p = calcP(h, l, c);
+    const ans = (2 * p) - h;
     return ans;
 }
 
@@ -107,6 +134,13 @@ function S2(h, l, c) {
     return ans;
 }
 
+function S3(h, l, c) {
+    const p = calcP(h, l, c);
+    const ans = l - 2*(h - p);
+    return ans;
+}
+
+//P
 function calcP(h, l, c) {
     const sum = h + l + c;
     const ans = sum / 3;
@@ -122,17 +156,22 @@ clear.addEventListener("click",()=>{
     const C = document.querySelector("#c").value="";
 
     const peint_name = document.querySelector(".name").textContent=`Calculation of `; // Assuming this is for display
-    const peint_P = document.querySelector(".P").textContent=``;
-    const peint_1R = document.querySelector(".R1").textContent=``;
-    const peint_1S = document.querySelector(".S1").textContent=``;
+    const peint_3R = document.querySelector(".R3").textContent=``;
     const peint_2R = document.querySelector(".R2").textContent=``;
+    const peint_1R = document.querySelector(".R1").textContent=``;
+    const peint_P = document.querySelector(".P").textContent=``;
+    const peint_1S = document.querySelector(".S1").textContent=``;
     const peint_2S = document.querySelector(".S2").textContent=``;
+    const peint_3S = document.querySelector(".S3").textContent=``;
 
-    const peint_ans_C_of_P_1R = document.querySelector(".c_of_p_r1").textContent=``;
-    const peint_ans_C_of_P_1S = document.querySelector(".c_of_p_s1").textContent=``;
-    const peint_P1 = document.querySelector(".P1").textContent=``;
+    //center
+    const peint_ans_C_of_2R_3R = document.querySelector(".c_of_r2_r3").textContent=``;
     const peint_ans_C_of_1R_2R = document.querySelector(".c_of_r1_r2").textContent=``;
+    const peint_ans_C_of_P_1R = document.querySelector(".c_of_p_r1").textContent=``;
+    const peint_P1 = document.querySelector(".P1").textContent=``;
+    const peint_ans_C_of_P_1S = document.querySelector(".c_of_p_s1").textContent=``;
     const peint_ans_C_of_1S_2S = document.querySelector(".c_of_s1_s2").textContent=``;
+    const peint_ans_C_of_2S_3S = document.querySelector(".c_of_s2_s3").textContent=``;
    
 
 })
